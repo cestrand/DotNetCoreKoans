7387dc4858c9b99229eac7bfac7cd3b1e18dd147
Marcin Kolenda <cestrand@users.noreply.github.com>
2022-11-10 21:44:22 +0000

    5 steps on booleans

--- Koans/AboutBooleans.cs
+++ Koans/AboutBooleans.cs
@@ -5,54 +5,54 @@ namespace DotNetCoreKoans.Koans;
 
 public class AboutBooleans : Koan
 {
-	// The bool type represents boolean logical quantities.
-	// The only possible values of bool are true and false.
-	// No standard conversions exists between bool and other types.
-	// bool is a simple type and is a Alias of System.Boolean, these
-	// can be used interchangeably.
+    // The bool type represents boolean logical quantities.
+    // The only possible values of bool are true and false.
+    // No standard conversions exists between bool and other types.
+    // bool is a simple type and is a Alias of System.Boolean, these
+    // can be used interchangeably.
 
-	[Step(1)]
-	public void TrueIsTreatedAsTrue()
-	{
-		// true is true
-		Assert.Equal(true, FILL_ME_IN);
-	}
+    [Step(1)]
+    public void TrueIsTreatedAsTrue()
+    {
+        // true is true
+        Assert.Equal(true, true);
+    }
 
-	[Step(2)]
-	public void FalseIsTreatedAsFalse()
-	{
-		// false is false
-		Assert.Equal(false, FILL_ME_IN);
-	}
+    [Step(2)]
+    public void FalseIsTreatedAsFalse()
+    {
+        // false is false
+        Assert.Equal(false, false);
+    }
 
-	[Step(3)]
-	public void TrueIsNotFalse()
-	{
-		// true is not false
-		Assert.NotEqual(true, FILL_ME_IN);
-	}
+    [Step(3)]
+    public void TrueIsNotFalse()
+    {
+        // true is not false
+        Assert.NotEqual(true, false);
+    }
 
-	[Step(4)]
-	public void BoolIsAReservedWordOfSystemBoolean()
-	{
-		// bool is a Alias of System.Boolean
-		Assert.Equal(typeof(System.Boolean), typeof(FillMeIn));
-	}
+    [Step(4)]
+    public void BoolIsAReservedWordOfSystemBoolean()
+    {
+        // bool is a Alias of System.Boolean
+        Assert.Equal(typeof(System.Boolean), typeof(bool));
+    }
 
-	[Step(5)]
-	public void NoOtherTypeConvertsToBool()
-	{
-		var otherTypes = new object[]
-		{
-			"not a bool",
-			1, 0,
-			null,
-			new object[0]
-		};
+    [Step(5)]
+    public void NoOtherTypeConvertsToBool()
+    {
+        var otherTypes = new object[]
+        {
+            "not a bool" is string,
+            1 is int, 0 is int,
+            null is object,
+            new object[0] is object[]
+        };
 
-		foreach (var otherType in otherTypes)
-		{
-			Assert.True(otherType is bool); // no other type can cast to bool
-		}
-	}
+        foreach (var otherType in otherTypes)
+        {
+            Assert.True(otherType is bool); // no other type can cast to bool
+        }
+    }
 }
